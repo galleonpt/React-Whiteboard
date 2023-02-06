6f12e165da86ad4e20a3993662da1759e53f8054
galleonpt <jose15.luis00@gmail.com>
2023-02-06 22:44:39 +0000

    first commit

---
+++ src/components/edges/DefaultEdge.tsx
@@ -0,0 +1,32 @@
+import { EdgeProps, getSmoothStepPath } from "reactflow";
+
+export default function DefaultEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  markerEnd,
+}: EdgeProps) {
+  const [edgePath] = getSmoothStepPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+
+  return (
+    <path
+      id={id}
+      style={style}
+      className="react-flow__edge-path stroke-2 stroke-zinc-300 "
+      d={edgePath}
+      markerEnd={markerEnd}
+    />
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
